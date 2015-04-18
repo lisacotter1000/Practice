4f9d53d7aea989d6cec16216c15577e4b952f0d7
Suzanne <sconsidine@hotmail.com>
2015-04-18 22:26:21 +0100

    2nd commit after adding the var3 = 122nd commit after adding the var3 = 12

--- src/main/java/com/workspace/arithmentic/addition.java
+++ src/main/java/com/workspace/arithmentic/addition.java
@@ -4,10 +4,11 @@ public class addition {
 
 	private static int var1 = 3;
 	private static int var2 = 6;
+	private static int var3 = 12;
 	
 	public static void main (String [] args){
 		
-		System.out.println(var1 + var2);
+		System.out.println(var1 + var2 + var3);
 		
 	}
 	

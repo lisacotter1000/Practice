b42f8ac7ec179a5ab8e6fea978e7f29030a22bdd
Suzanne <sconsidine@hotmail.com>
2015-04-18 22:06:12 +0100

    initial commit of the Arithmetic file

---
+++ src/main/java/com/workspace/arithmentic/addition.java
@@ -0,0 +1,15 @@
+package com.workspace.arithmentic;
+
+public class addition {
+
+	private static int var1 = 3;
+	private static int var2 = 6;
+	
+	public static void main (String [] args){
+		
+		System.out.println(var1 + var2);
+		
+	}
+	
+	
+}
